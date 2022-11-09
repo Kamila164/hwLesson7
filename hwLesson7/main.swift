59579de1e1952900656a2a4010b0ff32e8a252d8
kamila <murtazaeva.kamila01@gmail.com>
2022-11-09 15:58:07 +0600

    edit

--- hwLesson7/main.swift
+++ hwLesson7/main.swift
@@ -1,11 +1,18 @@
-//
-//  main.swift
-//  hwLesson7
-//
-//  Created by Kamila on 7/11/22.
-//
-
 import Foundation
 
-print("Hello, World!")
+var stringArray: [[String]] = [[],[]]
 
+var bool = false
+while bool == false{
+    var message = readLine()!
+    if message.count == 0{
+        bool = true
+    }else{
+        if Int(message) ?? 0 != 0 || Int(message) ?? 12 == 0{
+            stringArray[1].append(message)
+        }else{
+            stringArray[0].append(message)
+        }
+    }
+}
+print(stringArray)
